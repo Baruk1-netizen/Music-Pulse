bf8bae6d65af58f02179bb1a2c78a52cbc1feafb
Baruk1-netizen <134915896+Baruk1-netizen@users.noreply.github.com>
2024-09-18 01:00:00 +0300

    Pulse Models

--- db/models.py
+++ db/models.py
@@ -19,18 +19,6 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
-
-
-   
-   
- 
-   
-    
-    
-    
-    
-
-    
 class Package (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
