bb2adfe40ad19c1cf84a1585c91e3c798690f63e
Baruk1-netizen <134915896+Baruk1-netizen@users.noreply.github.com>
2024-09-18 00:56:11 +0300

    Pulse Models

--- db/models.py
+++ db/models.py
@@ -23,7 +23,7 @@ class User(db.Model):
 class Accommodation (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
+ 
    
     
     
