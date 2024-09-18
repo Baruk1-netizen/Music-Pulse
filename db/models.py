b306ae6ac13ab62c2300f11c18bf28f62e3cea17
Baruk1-netizen <134915896+Baruk1-netizen@users.noreply.github.com>
2024-09-18 13:25:55 +0300

    Pulse Models

--- db/models.py
+++ db/models.py
@@ -22,8 +22,8 @@ class User(db.Model):
 class Package (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    checkinDate = db.Column(db.DateTime, nullable=False)
+    
+    
     
     
     
